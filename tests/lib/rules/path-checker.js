f69b3608b23002a30d3cf12b8de5659854fe6450
marica <2cadmaster@gmail.com>
2023-08-22 21:36:18 +0300

    feat: add import layers rule

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -1,6 +1,6 @@
 /**
- * @fileoverview feature sliced relative path checker
- * @author marica
+ * @fileoverview descr
+ * @author timur
  */
 "use strict";
 
@@ -8,7 +8,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/path-checker"),
+const rule = require("../../../lib/rules/public-api-imports"),
   RuleTester = require("eslint").RuleTester;
 
 
@@ -19,30 +19,67 @@ const rule = require("../../../lib/rules/path-checker"),
 const ruleTester = new RuleTester({
   parserOptions: {ecmaVersion: 6, sourceType: 'module'}
 });
-ruleTester.run("path-checker", rule, {
+
+const aliasOptions = [
+  {
+    alias: '@'
+  }
+]
+
+ruleTester.run("public-api-imports", rule, {
   valid: [
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
       code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
       errors: [],
     },
+    {
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article'",
+      errors: [],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/entities/file.test.ts',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+      errors: [],
+      options: [{
+        alias: '@',
+        testFilePattens: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+      }],
+    },
+    {
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/entities/StoreDecorator.tsx',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+      errors: [],
+      options: [{
+        alias: '@',
+        testFilePattens: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+      }],
+    }
   ],
 
   invalid: [
     {
-      filename: '/Users/marica/Documents/frontend/react_proj/src/entities/Article',
-      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
-      errors: [{ message: "All pathes should  be relative in one slice"}],
-      options: [
-        {
-          alias: '@'
-        }
-      ]
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/file.ts'",
+      errors: [{ message: "Absolute import has been is allowed from public api"}],
+      options: aliasOptions,
     },
     {
-      filename: '/Users/marica/Documents/frontend/react_proj/src/entities/Article',
-      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
-      errors: [{ message: "All pathes should  be relative in one slice"}],
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/entities/StoreDecorator.tsx',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx'",
+      errors: [{message: 'Absolute import has been is allowed from public api'}],
+      options: [{
+        alias: '@',
+        testFilePattens: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+      }],
     },
+    {
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/entities/forbidden.ts',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+      errors: [{message: 'Testing data import has been is allowed from testing public api'}],
+      options: [{
+        alias: '@',
+        testFilePattens: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
+      }],
+    }
   ],
 });
